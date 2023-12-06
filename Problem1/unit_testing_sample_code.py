2a4c23d5855b4bfc7ff061c75f4512d84e38073e
Darling Rodriguez <drodriguez04@wesleyan.edu>
2023-12-06 13:58:45 -0500

    made corrections to problem 1

--- Problem1/unit_testing_sample_code.py
+++ Problem1/unit_testing_sample_code.py
@@ -1,10 +1,11 @@
 # COMP 333 Software Engineering
 # Wesleyan University
-# Alex Kaplan (akaplan01@wesleyan.edu) and Sebastian Zimmeck (szimmeck@wesleyan.edu)
 
 """
 Capitalizes the first and last character of a string.
 """
+
+
 def string_capitalizer(tobecapitalized):
     if tobecapitalized == "":
         return tobecapitalized
@@ -15,15 +16,18 @@ def string_capitalizer(tobecapitalized):
             if i == 0 or i == (len(tbclist) - 1):
                 tbclist[i] = tbclist[i].upper()
             i = i + 1
-        tobecapitalized = ''.join(tbclist)
+        tobecapitalized = "".join(tbclist)
         return tobecapitalized
     except:
         # If failure, return the input as is.
         return tobecapitalized
 
+
 """
 Capitalizes the first and last character of a string in a list of strings.
 """
+
+
 def capitalize_list(slist):
     i = 0
     while i < len(slist):
@@ -32,27 +36,34 @@ def capitalize_list(slist):
         i = i + 1
     return slist
 
+
 """
 Squares, doubles, and then floor divides (by 3) an integer, in that order.
 """
+
+
 def integer_manipulator(n):
     try:
-        n = ((n*n)*2)//3
+        n = ((n * n) * 2) // 3
         return n
     except:
         # If failure, return the input as is.
         return n
 
+
 """
 Squares, doubles, and then floor divides (by 3) an integer, in that order, in a
 list of integers.
 """
+
+
 def manipulate_list(intlist):
     i = 0
     while i < len(intlist):
         intlist[i] = integer_manipulator(intlist[i])
         i = i + 1
     return intlist
+
 
 # Sample function calls. Comment in to run.
 # print(string_capitalizer("hello"))
